68ff12c9226ae1d7d2721d173c219027ce53b131
Griffin Crane <grc4kd@users.noreply.github.com>
2022-04-10 14:16:23 -0500

    solution with api and ui projects

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace inventory_app_dotnet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class InventoryController : ControllerBase
+{
+    private readonly ILogger<InventoryController> _logger;
+
+    public InventoryController(ILogger<InventoryController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<Inventory> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Inventory
+        {
+            ID = index,
+            Name = "24697-013-01-05",
+            Kernels = 75
+        })
+        .ToArray();
+    }
+}
---
+++ Inventory.cs
@@ -0,0 +1,8 @@
+namespace inventory_app_dotnet;
+
+public class Inventory
+{
+    public int ID { get; set; }    
+    public string? Name { get; set; }
+    public int Kernels { get; set; }
+}
