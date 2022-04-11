4f97af721459db18d3e28f36afb66bb100321478
Griffin Crane <grc4kd@users.noreply.github.com>
2022-04-11 17:22:14 -0500

    fix app component spec

--- angular-ui/src/app/app.component.spec.ts
+++ angular-ui/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-ui'`, () => {
+  it(`should have as title 'Inventory App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ui');
+    expect(app.title).toEqual('Inventory App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-ui app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Inventory App');
   });
 });
