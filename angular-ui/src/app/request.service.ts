a2fbeb47cc1ce817f10be00361d19c33c254a8e3
Griffin Crane <grc4kd@users.noreply.github.com>
2022-04-12 17:38:59 -0500

    error handling for request service

--- angular-ui/src/app/request.service.ts
+++ angular-ui/src/app/request.service.ts
@@ -3,6 +3,7 @@ import { Request } from './request';
 import { REQUESTS } from './mock-requests';
 import { Observable, of } from 'rxjs'
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +16,38 @@ export class RequestService {
     private http: HttpClient) { }
 
   getRequests(): Observable<Request[]> {
-    return this.http.get<Request[]>(this.requestsUrl);
+    return this.http.get<Request[]>(this.requestsUrl)
+      .pipe(
+        catchError(this.handleError<Request[]>('getRequests', []))
+      );
   }
 
   getRequest(id: number): Observable<Request> {
-    // TODO: error handling when request(id) does not exist
-    const request = REQUESTS.find(r => r.id === id)!;
-    return of(request);
+    const url = `${this.requestsUrl}/${id}`;
+    return this.http.get<Request>(url).pipe(
+      // error handling when request(id) does not exist
+      catchError(this.handleError<Request>(`getRequest id=${id}`))
+    );
   }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+    private handleError<T>(operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+
+        // TODO: better job of transforming error for user consumption
+        console.error(`${operation} failed: ${error.message}`);
+
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+      };
+    }
 }
